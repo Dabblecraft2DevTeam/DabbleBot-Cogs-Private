0b614d8d2a5e270f2dca3c44186b3d24cb07b808
Yachtman25 <sarcasmman111@gmail.com>
2025-01-19 20:16:34 -0800

    Update DateChannelCog.py

--- DateChannel/DateChannelCog.py
+++ DateChannel/DateChannelCog.py
@@ -28,8 +28,8 @@ class DateChannelCog(commands.Cog):
             if channel:
                 try:
                     # Update the channel name with the current date
-                    await channel.edit(name=f"\uD83D\uDCC5｜{date_str}")
-                    print(f"Updated channel {channel.name} to \uD83D\uDCC5｜{date_str} in guild {guild.name}")
+                    await channel.edit(name=f"📅｜{date_str}")
+                    print(f"Updated channel {channel.name} to 📅｜{date_str} in guild {guild.name}")
                 except discord.Forbidden:
                     print(f"Bot does not have permission to edit the channel in guild {guild.name}.")
                 except discord.HTTPException as e:
